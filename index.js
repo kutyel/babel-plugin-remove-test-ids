8c1544eec6572d900671c5f12449cbffe700f8a1
kutyel <flaviocorpa@gmail.com>
2018-03-18 16:39:37 +0100

    Rename test file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = () => ({
   visitor: {
-    JSXAttribute: (path, { opts: { attributes = ['data-test-id'] }}) => 
+    JSXAttribute: (path, { opts: { attributes = ['data-test-id'] } }) =>
       attributes.includes(path.node.name.name) && path.remove()
   }
 })
